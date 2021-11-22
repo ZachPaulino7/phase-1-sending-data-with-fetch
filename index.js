f361d40e1787f620e6fa5a6dd95eb8edd0a14ee4
Zachpaulino7 <Zachp7@live.com>
2021-11-21 22:06:38 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(username, useremail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+       },
+        body: JSON.stringify({
+            name: `${username}`,
+            email: `${useremail}`,
+        })
+    })
+.then(response => response.json())
+.then(function newID(object){
+    document.body.innerHTML = object.id
+})
+.catch (function alertError(error) {
+    document.body.innerHTML = error.message
+})
+}
